0ec5f05c2ec12d72ccef0a685dc55791b936ee36
alexmorley <alexander.morley@univ.ox.ac.uk>
2019-01-06 15:13:39 +0100

    Change UI to accomodate authentication (auth not implemented)

---
+++ app/public/javascript/index.js
@@ -0,0 +1,9 @@
+function showLogin() {
+  let el = document.getElementById("login");
+  if(el.style.display == "block") {
+    el.style.display = "none"; 
+  } else {
+    el.style.display = "block"; 
+  }
+}
+
